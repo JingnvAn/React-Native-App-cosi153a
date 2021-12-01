0189b929476e3504cb7773176b4431c377a6fd21
Jingnu An <78512122+JingnvAn@users.noreply.github.com>
2021-12-01 16:07:23 -0500

    Basic functions on phone restored (#7)
    * add components and finish quiz4
    
    * make it work with navigation and on phone

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import NavigationStack from './components/NavigationStack';
-import Quiz2 from './practiceComponents/Quiz2';
-import Profile from './practiceComponents/Profile';
-import ExpenableText from './components/ExpenableText';
-import Quiz3 from './practiceComponents/Quiz3';
-import CovidDemo from './practiceComponents/CovidDemo';
-import Quiz4 from './practiceComponents/Quiz4';
+import { StyleSheet, Text, Button, View, ImageBackground, SafeAreaView } from 'react-native';
+// import NavigationStack from './components/NavigationStack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplitExpenseEditor from './components/SplitExpenseEditor'
+// import Setting from './components/Setting';
+// import ChatRoom from './components/ChatRoom';
+import Login from './components/Login';
+import Setting from './components/Setting';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Add A Split Transaction"
+        onPress={() => {
+          navigation.navigate('SplitScreen', {
+            me: 'Jingnu',
+            partner: 'Chris',
+          });
+        }}
+      />
+      <Button 
+        title="Setting"
+        onPress={() => {
+          navigation.navigate('SettingScreen', {
+            me: 'Jingnu',
+            partner: 'Chris',
+          });
+        }}
+      />
+    </View>
+  );
+}
+
+const SplitScreen = ({navigation, route}) => {
+  return (
+    <SplitExpenseEditor 
+      navigation={navigation} 
+      me={route.params.me} 
+      partner={route.params.partner} 
+    />
+  )
+}
+
+const LoginScreen = ({ navigation, route }) => {
+  return (
+    <Login 
+      navigation={navigation}
+     />
+  )
+}
+
+const SettingScreen = ({ navigation, route }) => {
+  return (
+    <Setting 
+      navigation={navigation}
+      name={route.params.me}
+    />
+  )
+};
 
 export default function App() {
   return(
-    <>
-      {/* <NavigationStack></NavigationStack> */}
-      {/* <ExpenableText title='A' text='this is the expanable text'></ExpenableText> */}
-      {/* <Quiz3></Quiz3> */}
-      <Quiz4 />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name ="Home" 
+          component={HomeScreen} 
+        />
+        <Stack.Screen name="SplitScreen" component={SplitScreen} options={{ title: 'Split Expenses'}} />
+        <Stack.Screen name='LoginScreen' component={LoginScreen} options={{ title: 'Log In'}}/>
+        <Stack.Screen name='SettingScreen' component={SettingScreen} options={{ title: 'Log In'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
   )
 }
 
@@ -29,5 +90,17 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     justifyContent: "center"
+  },
+  homeScreen:{
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
