5a4276794658571ba32e18166edd4eaf1ae07657
Jingnu An <78512122+JingnvAn@users.noreply.github.com>
2021-12-11 18:42:10 -0500

    Login firebase (#17)
    * change home page text
    
    * add login screen, add firebase
    
    * change button color
    
    * set initial screen to be login
    
    * update readme and add screenshots

---
+++ components/HomeScreen.js
@@ -0,0 +1,57 @@
+import { useNavigation } from '@react-navigation/core';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { auth } from '../firebase/config';
+
+const HomeScreen = () => {
+    const navigation = useNavigation()
+    const [user, setUser] = useState();
+    
+    useEffect(()=> {
+        setUser(auth.currentUser?.email);
+    }, [])
+
+    const handleSignOut = () => {
+        auth
+        .signOut()
+        .then(() => {
+            navigation.navigate("Login")
+        })
+        .catch(error => alert(error.message))
+    }
+    console.log('email',auth.currentUser?.email);
+    return (
+        <View style={styles.container}>
+        <Text>Email: {user}</Text>
+        <TouchableOpacity
+            onPress={handleSignOut}
+            style={styles.button}
+        >
+            <Text style={styles.buttonText}>Sign out</Text>
+        </TouchableOpacity>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+   button: {
+    backgroundColor: '#F194FF',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
