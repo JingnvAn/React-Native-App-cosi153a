ae7ba0d0c944205c03f8da05599b5b12de5b0b3e
Jingnu An <ajingnv@gmail.com>
2021-12-11 15:41:19 -0500

    add login screen, add firebase

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Setting from './components/Setting';
 import { Ionicons } from '@expo/vector-icons';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import SplitPretty from './components/SplitPretty';
-
+import HomeScreen from './components/HomeScreen';
+import LoginScreen from './components/LoginScreen';
 
 const Tab = createMaterialBottomTabNavigator();
+const Stack = createStackNavigator();
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={styles.title}>Welcome to Split!</Text>
-    </View>
-  );
-}
-
-const SplitScreen = ({navigation, route}) => {
+const SplitScreen = () => {
   return (
     <SplitPretty />
+  )
+}
+
+const Home = () => {
+  return (
+    <HomeScreen />
+  )
+}
+
+const Login = () => {
+  return (
+    <LoginScreen />
   )
 }
 
@@ -36,27 +43,28 @@ export default function App() {
   return(
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color }) => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings' : 'settings-outline';
-            } else if (route.name === 'Split'){
-              iconName = focused ? 'md-add-circle' : 'md-add-circle-outline';
-            }
-            return <Ionicons name={iconName} size={25} color={color} />;
-          },
-        })}
-        barStyle={{ backgroundColor: '#ffff' }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen}  />
-        <Tab.Screen name="Settings" component={SettingScreen} options={{ title: 'Settings'}} initialParams={{me:'Jingnu', partner:'Chris'}} />
-        <Tab.Screen name="Split" component={SplitScreen} options={{ title: 'Split Expenses'}} initialParams={{me:'Jingnu', partner: 'Chris'}} />
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          } else if (route.name === 'Split'){
+            iconName = focused ? 'md-add-circle' : 'md-add-circle-outline';
+          } 
+          return <Ionicons name={iconName} size={25} color={color} />;
+        },
+      })}
+      barStyle={{ backgroundColor: '#ffff' }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen}  />
+      <Tab.Screen name="Settings" component={SettingScreen} options={{ title: 'Settings'}} initialParams={{me:'Jingnu', partner: 'Chris'}} />
+      <Tab.Screen name="Split" component={SplitScreen} options={{ title: 'Split Expenses'}} initialParams={{me:'Jingnu', partner: 'Chris'}} />
+      <Tab.Screen name='Login' component={Login} />
       </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
